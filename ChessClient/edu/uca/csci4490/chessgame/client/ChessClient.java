72739f0a5c0547ca165d53a8be202ea04fb18ebe
Jasper Reddin <jasper@jasperreddin.com>
2020-10-27 16:38:29 -0500

    Create projects for client and server

---
+++ ChessClient/edu/uca/csci4490/chessgame/client/ChessClient.java
@@ -0,0 +1,10 @@
+package edu.uca.csci4490.chessgame.client;
+
+public class ChessClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ChessServer/edu/uca/csci4490/chessgame/server/ChessServer.java
@@ -0,0 +1,10 @@
+package edu.uca.csci4490.chessgame.server;
+
+public class ChessServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
