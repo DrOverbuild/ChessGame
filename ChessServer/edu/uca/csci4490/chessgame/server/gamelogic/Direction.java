3e41033e39406a70e8657434ca1f12de76089233
Jasper Reddin <jasper@jasperreddin.com>
2020-11-02 23:08:08 -0600

    Add ability to check if direction is L-shape, diagonal, or straight

--- ChessServer/edu/uca/csci4490/chessgame/server/gamelogic/Direction.java
+++ ChessServer/edu/uca/csci4490/chessgame/server/gamelogic/Direction.java
@@ -17,4 +17,30 @@ public enum Direction {
 	L_N3_W2,
 	L_S2_W3,
 	L_S3_W2;
+
+	public boolean isLShaped() {
+		return this == L_N2_E3 ||
+				this == L_N3_E2 ||
+				this == L_S2_E3 ||
+				this == L_S3_E2 ||
+				this == L_N2_W3 ||
+				this == L_N3_W2 ||
+				this == L_S2_W3 ||
+				this == L_S3_W2;
+
+	}
+
+	public boolean isDiagonal() {
+		return this == NORTHEAST ||
+				this == SOUTHEAST ||
+				this == NORTHWEST ||
+				this == SOUTHWEST;
+	}
+
+	public boolean isStraight() {
+		return this == NORTH ||
+				this == SOUTH ||
+				this == EAST ||
+				this == WEST;
+	}
 }
