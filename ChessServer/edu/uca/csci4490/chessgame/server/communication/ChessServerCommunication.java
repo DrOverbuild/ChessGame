816d6cd85ef19df43e1be6b350d8da4366661167
timuralimkhanov <timur.alimkhanof@gmail.com>
2020-11-23 18:36:59 -0600

    Classes for ChessServerCommunication and PlayerLoginCommunication

--- ChessServer/edu/uca/csci4490/chessgame/server/communication/ChessServerCommunication.java
+++ ChessServer/edu/uca/csci4490/chessgame/server/communication/ChessServerCommunication.java
@@ -4,6 +4,12 @@ import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
 
 public class ChessServerCommunication extends AbstractServer {
+	
+	//Data fields for Chess Server
+	//private PlayerLoginCommunication;
+	//private WaitingRoomCommuniciation;
+	//private GameCommunication;
+	
 	public ChessServerCommunication(int port) {
 		super(port);
 	}
@@ -12,4 +18,60 @@ public class ChessServerCommunication extends AbstractServer {
 	protected void handleMessageFromClient(Object o, ConnectionToClient connectionToClient) {
 
 	}
+	
+	public void listeningException(Throwable exception) {
+		
+	}
+	
+	public void serverStarted(){
+		
+	}
+	
+	public void serverStopped(){
+		
+	}
+	
+	public void serverClosed(){
+		
+	}
+	
+	public void clientConnected(){
+		
+	}
+	
+	public void setPlayerLoginCommunication(){
+		
+	}
+	
+	public void getPlayerLoginCommunication(){
+		
+	}
+	public void setWaitingRoomCommunication(){
+		
+	}
+	
+	public void getWaitingRoomCommunication(){
+	
+	}
+	
+	public void setGameCommunication() {
+		
+	}
+	
+	public void getGameCommunication() {
+		
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+//.
--- ChessServer/edu/uca/csci4490/chessgame/server/communication/PlayerLoginCommunication.java
+++ ChessServer/edu/uca/csci4490/chessgame/server/communication/PlayerLoginCommunication.java
@@ -1,4 +1,30 @@
 package edu.uca.csci4490.chessgame.server.communication;
 
+import edu.uca.csci4490.chessgame.model.Player;
+import edu.uca.csci4490.chessgame.model.data.*;
+import edu.uca.csci4490.chessgame.model.gamelogic.Game;
+import edu.uca.csci4490.chessgame.model.gamelogic.Location;
+import edu.uca.csci4490.chessgame.model.gamelogic.piece.Piece;
+import edu.uca.csci4490.chessgame.server.ChessServer;
+import ocsf.server.ConnectionToClient;
+
 public class PlayerLoginCommunication {
+	private ChessServer server;
+	/*
+	public void receiveCreateAccount(CreateAccountData data) {
+		
+	}
+	
+	public void receivePlayerLogin() {
+		
+	}
+	
+	public void sendLoginUnsuccessful() {
+		
+	}
+	
+	public void sendCreateAccountUnsuccessful () {
+		
+	}
+	*/
 }
